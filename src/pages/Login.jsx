07252066151f45373b3af97f44fc3815de7a0b5a
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-02-13 15:00:54 +0200

    finished styling the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Login = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className='login-container'>
+      <img src={logo} alt="logo" />
+      <div className='login-layout'>
+        <h1>Log in</h1>
+        <form action="">
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder='johndoe@gmail.com' />
+            <label htmlFor="">Password</label>
+            <input type="password" placeholder='..........' />
+        </form>
+        <p>forgot password?</p>
+        <button>GET IN</button>
+      </div>
     </div>
   )
 }
